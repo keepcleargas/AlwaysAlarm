7c29c3d41dd51607fe40c6747962a29b50a302d5
Tim Butram <atrus6@gmail.com>
2011-09-21 13:18:23 -0400

    Tuned the pedometer algorithm a bit. Hits steps fairly well...

--- src/org/worldsproject/alarmclock/AlarmRinging.java
+++ src/org/worldsproject/alarmclock/AlarmRinging.java
@@ -14,72 +14,79 @@ import android.widget.Toast;
 
 public class AlarmRinging extends Activity implements SensorEventListener
 {
-	private float   mLimit = 10;
-	private float   mLastValues[] = new float[3*2];
-	private float   mScale[] = new float[2];
-	private float   mYOffset;
-
-	private float   mLastDirections[] = new float[3*2];
-	private float   mLastExtremes[][] = { new float[3*2], new float[3*2] };
-	private float   mLastDiff[] = new float[3*2];
-	private int     mLastMatch = -1;
-
 	private double steps;
 
 	private MediaPlayer mMediaPlayer;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+
+	private TextView tv;
 
 	public void onCreate(Bundle savedInstance)
 	{
 		super.onCreate(savedInstance);
 		setContentView(R.layout.alarm_ringing);
+		mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE); 
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-//		Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-//		if(alert == null)
-//		{
-//			alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-//		}
-//		mMediaPlayer = new MediaPlayer();
-//		try
-//		{
-//			Log.v("TIM", "MediaPlayer is null: " + (mMediaPlayer == null));
-//			Log.v("TIM", "Alert is null: " + (alert == null));
-//			mMediaPlayer.setDataSource(this, alert);
-//			final AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-//			if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) 
-//			{
-//				mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
-//				mMediaPlayer.setLooping(true);
-//				mMediaPlayer.prepare();
-//				mMediaPlayer.start();
-//			}
-//		}
-//		catch (IllegalArgumentException e)
-//		{
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		catch (SecurityException e)
-//		{
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		catch (IllegalStateException e)
-//		{
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		catch (IOException e)
-//		{
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		SensorManager mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE); 
-		mSensorManager.registerListener(this,mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
-		
-		TextView tv = (TextView)this.findViewById(R.id.textView3);
+		//		Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+		//		if(alert == null)
+		//		{
+		//			alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+		//		}
+		//		mMediaPlayer = new MediaPlayer();
+		//		try
+		//		{
+		//			Log.v("TIM", "MediaPlayer is null: " + (mMediaPlayer == null));
+		//			Log.v("TIM", "Alert is null: " + (alert == null));
+		//			mMediaPlayer.setDataSource(this, alert);
+		//			final AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+		//			if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) 
+		//			{
+		//				mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+		//				mMediaPlayer.setLooping(true);
+		//				mMediaPlayer.prepare();
+		//				mMediaPlayer.start();
+		//			}
+		//		}
+		//		catch (IllegalArgumentException e)
+		//		{
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
+		//		catch (SecurityException e)
+		//		{
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
+		//		catch (IllegalStateException e)
+		//		{
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
+		//		catch (IOException e)
+		//		{
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
+
+
+		tv = (TextView)this.findViewById(R.id.textView3);
 		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
 		steps = Integer.parseInt(pref.getString("steps", "20"));
 		tv.setText("" + steps);
+	}
+
+	protected void onResume() 
+	{
+		super.onResume();
+		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	protected void onPause() 
+	{
+		super.onPause();
+		mSensorManager.unregisterListener(this);
 	}
 
 	@Override
@@ -92,67 +99,32 @@ public class AlarmRinging extends Activity implements SensorEventListener
 	@Override
 	public void onSensorChanged(SensorEvent event)
 	{
-		Sensor sensor = event.sensor; 
-		synchronized (this) 
-		{
-			if (sensor.getType() == Sensor.TYPE_ORIENTATION) 
+			float x = event.values[0];
+			float y = event.values[1];
+			float z = event.values[2];
+			
+			double xs = Math.abs(Math.sqrt(x*x));
+			double ys = Math.abs(Math.sqrt(y*y));
+			double zs = Math.abs(Math.sqrt(z*z));
+			double speed = (Math.sqrt(x*x + y*y + z*z) - SensorManager.GRAVITY_EARTH);
+			
+			int rightDir = 0;
+			if(xs > 0.5)
+				rightDir++;
+			if(ys > 0.5)
+				rightDir++;
+			if(zs > 0.5)
+				rightDir++;
+				
+			if(Math.abs(speed) > .85 && rightDir > 1)
 			{
-
+				steps--;
+				
+				if(steps <= 0)
+					this.finish();
+				
+				tv.setText("" + steps);
 			}
-			else
-			{
-				int j = (sensor.getType() == Sensor.TYPE_ACCELEROMETER) ? 1 : 0;
-				if (j == 1) 
-				{
-					Toast.makeText(getBaseContext(), "On Sensor Changed", Toast.LENGTH_SHORT).show();
-					float vSum = 0;
-					for (int i=0 ; i<3 ; i++) 
-					{
-						final float v = mYOffset + event.values[i] * mScale[j];
-						vSum += v;
-					}
-
-					int k = 0;
-					float v = vSum / 3;
-
-					float direction = (v > mLastValues[k] ? 1 : (v < mLastValues[k] ? -1 : 0));
-
-					if (direction == - mLastDirections[k]) 
-					{
-						// Direction changed
-						int extType = (direction > 0 ? 0 : 1); // minumum or maximum?
-						mLastExtremes[extType][k] = mLastValues[k];
-						float diff = Math.abs(mLastExtremes[extType][k] - mLastExtremes[1 - extType][k]);
-
-						if (diff > mLimit) 
-						{
-							boolean isAlmostAsLargeAsPrevious = diff > (mLastDiff[k]*2/3);
-							boolean isPreviousLargeEnough = mLastDiff[k] > (diff/3);
-							boolean isNotContra = (mLastMatch != 1 - extType);
-
-							if (isAlmostAsLargeAsPrevious && isPreviousLargeEnough && isNotContra) 
-							{
-								steps -= 0.5;
-
-								if(steps <= 0)
-								{
-									//Stop alarm.
-									Toast.makeText(getBaseContext(), "Step Taken", Toast.LENGTH_SHORT).show();
-								}
-								mLastMatch = extType;
-							}
-							else 
-							{
-								mLastMatch = -1;
-							}
-						}
-						mLastDiff[k] = diff;
-					}
-					mLastDirections[k] = direction;
-					mLastValues[k] = v;
-				}
-			}
-		}
 	}
 
 }
